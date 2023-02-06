0767a1a5b8b6262ca1c5b9a9dabe6d6f3ec4a9db
SungHo Ahn <jugiaro95@gmail.com>
2023-02-06 23:11:30 +0900

    :o: study vlpt redux 6-6

---
+++ learn-redux/src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// import "./exercise";
+import { createStore } from "redux";
+import rootReducer from "./modules";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension"; // 리덕스 개발자 도구
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const store = createStore(rootReducer, composeWithDevTools());
+// console.log("index.js", store.getState());
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
