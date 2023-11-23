8437be5a19d78c50506ec541b94863ba21b2aaf4
SungHo Ahn <jugiaro95@gmail.com>
2023-11-23 23:28:14 +0900

    [webpack] :o: 02-06. 자주 사용하는 로더

--- webpack/sample/src/app.js
+++ webpack/sample/src/app.js
@@ -1,3 +1,12 @@
 import * as math from "./math.js";
+import "./app.css";
+import nyancat from "./nyancat.jpg";
 
 console.log(math.sum(1, 2));
+
+document.addEventListener("DOMContentLoaded", () => {
+  // DOMContentLoaded: DOM이 로딩됬을 때
+  document.body.innerHTML = `
+        <img src="${nyancat}" />
+    `;
+});
--- webpack/sample/webpack.config.js
+++ webpack/sample/webpack.config.js
@@ -17,6 +17,19 @@ module.exports = {
           path.resolve("./my-webpack-loader.js"), // 사용할 로더 명시
         ],
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"], // 뒤부터 loader가 실행된다.
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "url-loader",
+        options: {
+          publicPath: "./dist", // file-loader가 처리하는 파일을 모듈로 사용할 때, 그 경로 앞에 추가되는 문자 (index.html 입장에서 이미지 파일은 src 폴더에 있는거여서 경로 표기가 필요)
+          name: "[name].[ext]?[hash]", // file-loader가 파일을 아웃풋에 복사할 때 사용하는 파일 이름
+          limit: 20000, // = 20KB, 파일 용량 설정 가능 (20KB미만인 파일은 url-loader로 base64로 변환하는 의미(= 문자열로), 20KB 이상인 경우 file-loader가 실행(= 파일로))
+        },
+      },
     ],
   },
 };
